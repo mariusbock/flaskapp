88cb30f4baadcdab07528af1cedf2a99ab37b672
Margareta Puscas <marmarpuscas@gmail.com>
2019-11-24 13:49:18 +0200

    endpoint for grab-missing-data added in file "routes.py"

--- project/recipes/routes.py
+++ project/recipes/routes.py
@@ -5,7 +5,7 @@ from project.models import TrainData
 from apscheduler.schedulers.background import BackgroundScheduler
 import pandas as pd
 import requests
-
+from project.protocol import Protocol
 from . import recipes_blueprint
 
 
@@ -144,6 +144,16 @@ def update_models():
     """ Here would the call of the Backend API happen, needs to be implemented with Victor"""
     print("If this would work it would update models!")
 
+@recipes_blueprint.route('/grab-missing-data', methods=['POST'])
+def grab_missing_data():
+    response = request.get("api from server")
+    protocol = Protocol(response.json())
+    return protocol.send_response_to_server()
+    # listOfIterogations = []
+    # typeInterrogation = {"type": "status", "fields": ["id", "datetime", "occupancy", "vehicle_flow", "timestamp"],
+    #                      "rule": ""}
+    # listOfIterogations.append(typeInterrogation)
+    # requestFromServer = {"requestId": 1, "listOfInterogations": listOfIterogations, "matchingRule": {}}
 """
 Following code is the scheduler that executes the update model function periodically. Currently only skeleton with no
 function.
