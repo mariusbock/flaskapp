a5d61db89a0f46ff11e4c7b42f1acc3e93a762e3
Victor <curean_victor@yahoo.com>
2019-12-01 20:18:12 +0200

    Fixed model test + preprocessed data test

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,7 +1,7 @@
 """
 This file (test_models.py) contains all unit tests for the models.py file
 """
-
+from project.protocol import Protocol
 
 def test_new_traindata(new_traindata):
     """
@@ -14,5 +14,7 @@ def test_new_traindata(new_traindata):
     assert new_traindata.id == "18371007[D40a]"
     assert new_traindata.last_occupancy == 16.5
     assert new_traindata.last_1_occupancy == 6.6667
-    # assert new_traindata.last_5_oocupancy == 14.5
-    # assert new_traindata.oocupancy == 15.5
+    assert new_traindata.last_5_occupancy == 14.5
+    assert new_traindata.occupancy == 15.5
+
+
