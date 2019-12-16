bf7f7c3997c1840db6f8eed727fa9fd5ce3b3e4c
Victor <curean_victor@yahoo.com>
2019-12-16 20:53:12 +0200

    Created master endpoint, cleaned routes.py

---
+++ project/controller/request.py
@@ -0,0 +1,55 @@
+from sqlalchemy import MetaData, Table, Column, Integer, String
+
+from project import db
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import pandas as pd
+
+Base = declarative_base()
+
+
+class Request(Base):
+    __tablename__ = "requests"
+    request_id = db.Column(db.String, primary_key=True)
+    client_id = db.Column(db.String)
+    raw_data = db.Column(db.String)
+    meta_data = db.Column(db.String)
+    request_status = db.Column(db.String)
+
+    def __init__(self, request_id, client_id, raw_data, meta_data):
+        self.request_id = request_id
+        self.client_id = client_id
+        self.raw_data = raw_data
+        self.meta_data = meta_data
+        self.request_status = "INITIALIZED"
+
+        save_request_to_db(self)
+
+    def serialize(self):
+        return {"request_id":self.request_id,
+                "client_id":self.client_id,
+                "request_status":self.request_status}
+
+def save_request_to_db(request):
+    Session = sessionmaker(bind=db.engine)
+    session = Session()
+    session.add(request)
+
+def get_request_by_id(request_id):
+    if not db.engine.dialect.has_table(db.engine, "requests"):  # If table don't exist, Create.
+        print("CREATING TABLE\n")
+        metadata = MetaData(db.engine)
+        # Create a table with the appropriate Columns
+        Table("requests", metadata,
+              Column('request_id', String, primary_key=True, nullable=False),
+              Column('client_id', String),
+              Column('raw_data', String),
+              Column('meta_data', String),
+              Column('request_status', String))
+              # Implement the creation
+        metadata.create_all()
+
+    Session = sessionmaker(bind=db.engine)
+    session = Session()
+    result = session.query(Request).first()
+    return result
---
+++ project/recipes/devroutes.py
@@ -0,0 +1,129 @@
+from project import db
+from flask import make_response, jsonify, request, Response, render_template
+from project.predictions import predict_occupancy
+from project.models import TrainData
+from apscheduler.schedulers.background import BackgroundScheduler
+import pandas as pd
+import requests
+from project.model.type_interrogation import *
+from . import recipes_blueprint
+from project.controller.protocol_cotroller import *
+from project.controller.table_controller import *
+from project.controller.request import Request
+
+@recipes_blueprint.route('/predictOccupancy', methods=['POST'])
+def api_predict_occupancy():
+    """
+    POST method predictOccupancy that returns the predicted occupancy using the latest trained model for the given
+    parsed_data. Needs to have a JSON attached in the body following the needed format.
+    :return: Returns a JSON file containing the predicted occupancy for the given parsed_data
+    :raises: 415 Unsupported Media Type if it is not a JSON in the body
+    """
+    if request.headers['Content-Type'] == 'application/json':
+
+        data = pd.DataFrame(request.json)
+        print(data)
+        # TODO: Exception Handling of when JSON does not follow correct format
+        processed_data = predict_occupancy(data)
+
+        resp = jsonify(processed_data)
+
+        resp.status_code = 200
+
+        return resp
+    else:
+        return "415 Unsupported Media Type"
+
+@recipes_blueprint.route('/saveToDB', methods=['POST'])
+def save_to_db():
+    if request.headers['Content-Type'] == 'application/json':
+
+        data = pd.DataFrame(request.json)
+        print(data)
+
+        data.to_sql('TrainData', con=db.engine, index=False, if_exists="replace")
+
+        return "All train data saved to DB."
+    else:
+        return "415 Unsupported Media Type"
+
+@recipes_blueprint.route('/getDataFromDB', methods=['GET'])
+def get_data_from_db():
+    data = pd.read_sql(sql=db.session.query(TrainData)
+                       .with_entities(TrainData.timestamp,
+                                      TrainData.id,
+                                      TrainData.last_occupancy,
+                                      TrainData.last_1_occupancy,
+                                      TrainData.last_5_occupancy,
+                                      TrainData.occupancy).statement,
+                       con=db.session.bind)
+
+    resp = Response(response=data.to_json(orient='records'),
+                    status=200,
+                    mimetype="application/json")
+
+    return resp
+
+@recipes_blueprint.route('/echo', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+
+    elif request.method == 'POST':
+        return "ECHO: POST\n"
+
+    elif request.method == 'PATCH':
+        return "ECHO: PACTH\n"
+
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE"
+
+@recipes_blueprint.route('/grab-missing-data', methods=['POST'])
+def grab_missing_data():
+    print("PROCESS REQUEST FROM JAVA SERVER:\n")
+    protocolController = ProtocolController()
+    return protocolController.grab_missing_data()
+
+@recipes_blueprint.route('/save-table', methods=['POST'])
+def save_table_to_db():
+    data = pd.DataFrame({"type":[request.json]})
+    tableController = TableController()
+    if request.headers['Content-Type'] == 'application/json':
+        tableController.save_to_db(data)
+        return "Table saved into the Db."
+    else:
+        return "415 Unsupported Media Type"
+
+@recipes_blueprint.route('/get-table', methods=['GET'])
+def get_table_from_db():
+    tableController = TableController()
+    return tableController.get_data_from_db()
+
+@recipes_blueprint.route('/insert-values', methods=['POST'])
+def insert_into_table():
+    data = pd.DataFrame(request.json)
+    tableController = TableController()
+    if request.headers['Content-Type'] == 'application/json':
+        tableController.insert_to_db(data)
+        return "Values inserted into the table."
+    else:
+        return "415 Unsupported Media Type"
+
+# @recipes_blueprint.route('/grab-missing-data', methods=['POST'])
+# def grab_missing_data():
+#     print("PROCESS REQUEST FROM JAVA SERVER:\n")
+#     print(request.get_json())
+#
+#     response = request.get_json()
+#     protocol = Protocol(response)
+#     return protocol.send_response_to_server()
+# """
+# Following code is the scheduler that executes the update model function periodically. Currently only skeleton with no
+# function.
+# """
+# sched = BackgroundScheduler(daemon=True)
+# sched.add_job(update_models, 'interval', seconds=100)
+# sched.start()
